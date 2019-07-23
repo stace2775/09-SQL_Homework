ee9db179356b46957d957f744f0dacdcbf3ab906
Stacey <stacey.ellen.smith@gmail.com>
2019-07-23 08:34:22 -0500

    initial commit

---
+++ 09_SQL_Homework_Deployment_Script.sql
@@ -0,0 +1,19 @@
+/*  
+ 
+ Description:
+	License Manager Deployment Script.
+
+	THIS SCRIPT WILL DROP ALL TABLES AND DELETE ALL DATA RELATED TO License Manager PersonalInformation.
+
+ Modification Summary:
+ DD-MMM-YYYY     Author              Description
+ 18-Aug-2016     Stacey Smith		Drop/Add License Manager PersonalInformation table.
+									Create PersonalInformation table.
+									Drop/Create usp_LM_Contacts_INSERT.
+									Drop/Create usp_LM_Contacts_UPDATE.
+									Drop/Create usp_LM_Contacts_GET.
+																		
+*/  
+
+--BEGIN DEPLOYMENT SCRIPT
+--========================================================================================
