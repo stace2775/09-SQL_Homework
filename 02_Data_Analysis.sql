b154883107f054921332d4f5e6e5bdd6d7a88c6f
Stacey <stacey.ellen.smith@gmail.com>
2019-07-24 09:42:23 -0500

    updates to queries

--- 02_Data_Analysis.sql
+++ 02_Data_Analysis.sql
@@ -36,8 +36,9 @@ SELECT
 	DM.dept_no AS Department_Number,
 	D.dept_name AS Department_Name,
 	DM.emp_no AS Employee_Number,
-	E.last_name AS Mgr_Last_Name,
-	E.first_name AS Mgr_First_Name,
+	E.last_name || ', ' || E.first_name AS Mgr_Full_Name,
+	--E.last_name AS Mgr_Last_Name,
+	--E.first_name AS Mgr_First_Name,
 	DE.from_date AS Start_Date,
 	DE.to_date AS End_Date
 
@@ -53,8 +54,9 @@ ON E.emp_no = DE.emp_no;
 --List the department of each employee with the following information: employee number, last name, first name, and department name.
 SELECT 
 	E.emp_no AS Employee_Number,
-	E.last_name AS Last_Name,
-	E.first_name AS First_Name,
+	E.last_name || ', ' || E.first_name AS Full_Name,
+	--E.last_name AS Last_Name,
+	--E.first_name AS First_Name,
 	D.dept_name AS Department_Name
 FROM emp E
 JOIN dept_emp DE
@@ -63,24 +65,25 @@ JOIN dept D
 ON D.dept_no = DE.dept_no;
 
 --List all employees whose first name is "Hercules" and last names begin with "B."
-SELECT 
-	first_name,
-	last_name
-FROM emp
+SELECT E.last_name || ', ' || E.first_name AS Full_Name
+	--first_name,
+	--last_name
+FROM emp E
 WHERE first_name = 'Hercules' and last_name like 'B%';
 
 
 --List all employees in the Sales department, including their employee number, last name, first name, and department name.
 SELECT
-	E.emp_no,
-	E.last_name,
-	E.first_name,
+	DE.emp_no,
+	E.last_name || ', ' || E.first_name AS Full_Name,
+	--E.last_name,
+	--E.first_name,
 	D.dept_name
-FROM emp E
-JOIN dept_mgr DM
-ON E.emp_no = DM.emp_no
+FROM dept_emp DE
 JOIN dept D
-ON D.dept_no = DM.dept_no
+ON D.dept_no = DE.dept_no
+JOIN emp E
+ON E.emp_no = DE.emp_no
 WHERE D.dept_name = 'Sales';
 
 
